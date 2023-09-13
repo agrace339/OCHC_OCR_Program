ea192183ed039e0ca5c90df04b3c0aa596707b26
Anna Grace <41017507+agrace339@users.noreply.github.com>
2023-09-13 16:00:25 -0400

    Add files via upload

---
+++ EasyOCR_test.py
@@ -0,0 +1,18 @@
+import easyocr
+import os
+
+def makeTextFile(file_name):
+	output_name = file_name.split(".")[0] + ".txt"
+	print(output_name)
+	
+	result = reader.readtext(file_name, detail = 0, rotation_info = [90, 180 ,270], 
+									slope_ths = .5, width_ths = .6, height_ths = .6)
+	print(result)
+
+reader = easyocr.Reader(['en'], gpu = True)
+path = r"/Users/annagrace/Desktop/Python OCR"
+dir_list = os.listdir(path)
+
+for i in range(len(dir_list)):
+	if ".jpg" in dir_list[i]:
+		makeTextFile(dir_list[i])
