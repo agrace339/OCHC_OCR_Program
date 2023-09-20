8c936231916c5b553b19f42af26632658137785a
Anna Grace <41017507+agrace339@users.noreply.github.com>
2023-09-20 10:12:05 -0400

    Update EasyOCR_test.py

--- EasyOCR_test.py
+++ EasyOCR_test.py
@@ -5,8 +5,7 @@ def makeTextFile(file_name):
 	output_name = file_name.split(".")[0] + ".txt"
 	print(output_name)
 	
-	result = reader.readtext(file_name, detail = 0, rotation_info = [90, 180 ,270], 
-									slope_ths = .5, width_ths = .6, height_ths = .6)
+	result = reader.readtext(file_name, detail = 0, rotation_info = [90, 180 ,270], slope_ths = .5, width_ths = .6, height_ths = .6)
 	print(result)
 
 reader = easyocr.Reader(['en'], gpu = True)
