9c85a4143d1f9b5b591c1f52d0ffd5bbe3e64f89
zcarovan <63746631+zcarovan@users.noreply.github.com>
2023-09-19 22:51:40 -0400

    Creates Tkinter window

---
+++ batch_ui.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+def main():
+	#Create Tkinter window.
+	window = tk.Tk()
+	greeting = tk.Label(text="Hello, Tkinter")
+
+	#We won't keep this code later, just meant for 
+	#keeping the window up for now.
+	input_thing = input()
+
+if __name__ == '__main__':
+	main()
