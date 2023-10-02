437714d9447aee8595737006b2d8fd13f758f21c
dkaruri <98552662+dkaruri@users.noreply.github.com>
2023-10-01 21:07:10 -0400

    Update batch_ui.py
    Writes file paths to example.txt once closed.

--- batch_ui.py
+++ batch_ui.py
@@ -5,8 +5,13 @@ from TkinterDnD2 import DND_FILES, TkinterDnD
 #Install TkinterDnD2 in the \Lib\site-packages of Python install. https://sourceforge.net/projects/tkinterdnd/
 
 #Drag and drop information and code provided by https://stackoverflow.com/questions/25427347/how-to-install-and-use-tkdnd-with-python-tkinter-on-osx/46856247#46856247
+
+text_file = open("example.txt", "w")
+
 def drop_in(event):
-	list_box.insert("end", event.data)
+	list_box.insert("end", event.data)	
+	text_file.write(event.data)
+	text_file.write("\n")
 
 #Create Tkinter window.
 window = TkinterDnD.Tk()
@@ -17,5 +22,7 @@ list_box = tk.Listbox(window, selectmode=tk.SINGLE, background="White")
 list_box.pack(fill=tk.X)
 list_box.drop_target_register(DND_FILES)
 list_box.dnd_bind("<<Drop>>", drop_in)
-	
+
 window.mainloop()
+
+text_file.close()
