70d7befd8d63a95e0b695422f64b03469d9a3f55
zcarovan <63746631+zcarovan@users.noreply.github.com>
2023-10-09 23:03:27 -0400

    Fully connected to drag and drop UI

--- batch_conversion.py
+++ batch_conversion.py
@@ -10,7 +10,10 @@ def file_to_pdf(file):
 	#CITE: https://github.com/josch/img2pdf
 	#DESC: Open source code from IMG2PDF github
 	#Open input file and convert to pdf.
-	with open("name.pdf","wb") as f:
+	match = re.search(r"(\w)+.(jpg|tif)", file)
+	if match:
+		title = (match.group())[0:-4] + ".pdf"
+	with open(title,"wb") as f:
 		f.write(img2pdf.convert(file))
 
 def folder_to_pdf(folder_name):
@@ -31,31 +34,30 @@ def folder_to_pdf(folder_name):
 	with open("name.pdf","wb") as f:
 		f.write(img2pdf.convert(imgs))
 
-def main(files):
-	#If no JPEG or TIFF file given, raise error.
-	#print(files.type())
-	#if () <= 1):
-	#	raise Exception("No file given.")
-
-	print(files[0])
-
-	#Take argument from command line as input file.
-	in_element = files[0]
+def convert(file):
 
 	#Regular expressions to determine if input element is file or folder.
-	in_file = re.match(r"[/a-zA-Z0-9]+.(jpg|tif)", in_element)
-	in_folder = re.match(r"/[a-zA-Z0-9]+", in_element)
+	in_file = re.match(r"[/a-zA-Z0-9]+.(jpg|tif)", file)
+	in_folder = re.match(r"/[a-zA-Z0-9]+", file)
 
 	#If single file given, convert just that file to a pdf.
 	if in_file:
-		file_to_pdf(in_element)
+		file_to_pdf(file)
 	#If folder is given, convert all image files to pdfs in folder.
 	elif in_folder:
-		folder_to_pdf(in_element)
+		folder_to_pdf(file)
 	#If argument is invalid, raise exception.
 	else:
 		raise Exception("Not valid file or folder name.")
 
+def main(files):
+	#If no JPEG or TIFF file given, raise error.
+	if not files:
+		raise Exception("No file given.")
+	
+	#Convert all files from list.
+	for file in files:
+		convert(file)
 
 if __name__ == '__main__':
 	main()
