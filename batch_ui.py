bece4201e1252fc8b80d213d26e70190a84f1d08
zcarovan <63746631+zcarovan@users.noreply.github.com>
2023-10-10 21:46:17 -0400

    When convert button is pressed, goes to new OCR window

--- batch_ui.py
+++ batch_ui.py
@@ -7,19 +7,33 @@ import batch_conversion
 
 #Drag and drop information and code provided by https://stackoverflow.com/questions/25427347/how-to-install-and-use-tkdnd-with-python-tkinter-on-osx/46856247#46856247
 
+# When a file is dragged into the box, the file path is dropped into the list box.
 def drop_in(event):
 	list_box.insert("end", event.data)	
 
+#Converts files and displays pop-up message when complete.
 def convert():
-	batch_conversion.main(list_box.get('0', 'end'))
-	pop_up("Files successfully converted!")
+	batch_conversion.main(list_box.get(0, 'end'))
+	#pop_up("Files successfully converted!")
+	ocr_page()
 
+#Creates OCR page.
+def ocr_page():
+	#Destroys drag and drop page.
+	list_box.destroy()
+	convert_button.destroy()
+	#Creates new OCR page.
+	window.title("OCR processing")
+
+# Creates pop up window to confirm the files were successfully converted.
 def pop_up(message):
+	#Creates small pop-up window.
 	popUpWindow = TkinterDnD.Tk()
 	popUpWindow.configure(bg = "#d9d9d9")
 	popUpWindow.geometry("300x100")
 	textMessage = tk.Label(popUpWindow, text=message)
 	textMessage.pack(side="top", fill="x", pady=10)
+	#Creates button to exit the pop-up window.
 	confirmButton = tk.Button(popUpWindow, text="Confirm", command = popUpWindow.destroy)
 	confirmButton.pack()
 	popUpWindow.mainloop()
@@ -29,8 +43,8 @@ window = TkinterDnD.Tk()
 window.configure(bg = "#d9d9d9")
 window.geometry("800x600")
 window.title("Drag and drop your files below.")
-#greeting = tk.Label(text="Hello, Tkinter")
 
+#Creates drag and drop list box.
 list_box = tk.Listbox(window, selectmode=tk.SINGLE, background="#999999")
 list_box.pack(fill=tk.X)
 list_box.drop_target_register(DND_FILES)
@@ -38,7 +52,7 @@ list_box.dnd_bind("<<Drop>>", drop_in)
 file_location = [list_box.get(0, last=None)]
 
 #If convert button is pressed, get contents of listbox and convert.
-convert_button = tk.Button(window, text= "Hello!", command = convert, background = "#3c78d8")
+convert_button = tk.Button(window, text= "Convert", command = convert, background = "#3c78d8")
 convert_button.place(x=400, y=500)
 
 window.mainloop()
