96f1933a6b028c4c8eecbeffd9ef937e9b36fb34
Anna Grace <41017507+agrace339@users.noreply.github.com>
2023-11-06 11:57:53 -0500

    Successful UploadFile

--- google_document_ai.py
+++ google_document_ai.py
@@ -97,19 +97,23 @@ class DocumentAI:
         bucket = storage_client.bucket(bucket_name)
         blob = bucket.blob(destination_blob_name)
 
-        # Optional: set a generation-match precondition to avoid potential race conditions
-        # and data corruptions. The request to upload is aborted if the object's
-        # generation number does not match your precondition. For a destination
-        # object that does not yet exist, set the if_generation_match precondition to 0.
-        # If the destination object already exists in your bucket, set instead a
-        # generation-match precondition using its generation number.
-        generation_match_precondition = 0
+        if not blob.exists(storage_client):
 
-        blob.upload_from_filename(
-            source_file_name, if_generation_match=generation_match_precondition
-        )
+            # Optional: set a generation-match precondition to avoid potential race conditions
+            # and data corruptions. The request to upload is aborted if the object's
+            # generation number does not match your precondition. For a destination
+            # object that does not yet exist, set the if_generation_match precondition to 0.
+            # If the destination object already exists in your bucket, set instead a
+            # generation-match precondition using its generation number.
+            generation_match_precondition = 0
 
-        print(f"File {source_file_name} uploaded to {destination_blob_name}.")
+            blob.upload_from_filename(
+                source_file_name, if_generation_match=generation_match_precondition
+            )
+
+            print(f"File {source_file_name} uploaded to {destination_blob_name}.")
+        else:
+            print(f"File {source_file_name} already in {bucket_name}.")
 
 
     def __batchProcessFiles(
