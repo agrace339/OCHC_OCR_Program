c904c1289b0c1a4e76cee3c78e24e8e690c573a3
Anna Grace <41017507+agrace339@users.noreply.github.com>
2023-11-06 16:31:35 -0500

    Update google_document_ai.py

--- google_document_ai.py
+++ google_document_ai.py
@@ -11,7 +11,7 @@ from google.api_core.exceptions import RetryError
 from google.cloud import documentai
 from google.cloud import storage
 
-#Class to access all Google Document AI-related functions.
+# Class to access all Google Document AI-related functions.
 class DocumentAI:
     global PROJECT_ID
     global LOCATION
@@ -56,7 +56,7 @@ class DocumentAI:
         for i in range(len(dir_list)):
             self.transcribeFile(folder_path + "/" + dir_list[i], output_type)
     
-    #Transcribes individual PDF file.
+    # Transcribes individual PDF file.
     def transcribeFile(self, file_path, output_type):
         name = file_path.split("/")[-1]
         # Refer to https://cloud.google.com/document-ai/docs/file-types for supported file types
@@ -106,8 +106,9 @@ class DocumentAI:
         with open(output_name, "w") as text_file:
             text_file.write(document_object.text)
 
-
+    # Uploads files to GCS Bucket
     def __uploadFiles(self, bucket_name, source_file_name, destination_blob_name):
+        # Creates client connection to a bucket
         storage_client = storage.Client()
         bucket = storage_client.bucket(bucket_name)
         blob = bucket.blob(destination_blob_name)
@@ -120,13 +121,14 @@ class DocumentAI:
         # generation-match precondition using its generation number.
         generation_match_precondition = 0
 
+        # Uploads file to bucket
         blob.upload_from_filename(
             source_file_name, if_generation_match=generation_match_precondition
         )
 
         print(f"File {source_file_name} uploaded to {destination_blob_name}.")
 
-
+    # Performs batch transcription on a specificied GCS bucket
     def __batchProcessFiles(
         self,
         gcs_output_uri: str,
@@ -137,16 +139,13 @@ class DocumentAI:
         field_mask: Optional[str] = None,
         timeout: int = 400,
     ) -> None:
-        # You must set the `api_endpoint` if you use a location other than "us".
         opts = ClientOptions(api_endpoint=f"{LOCATION}-documentai.googleapis.com")
 
         client = documentai.DocumentProcessorServiceClient(client_options=opts)
 
         if gcs_input_uri:
             # Specify specific GCS URIs to process individual documents
-            gcs_document = documentai.GcsDocument(
-                gcs_uri=gcs_input_uri, mime_type=input_mime_type
-            )
+            gcs_document = documentai.GcsDocument(gcs_uri=gcs_input_uri, mime_type=input_mime_type)
             # Load GCS Input URI into a List of document files
             gcs_documents = documentai.GcsDocuments(documents=[gcs_document])
             input_config = documentai.BatchDocumentsInputConfig(gcs_documents=gcs_documents)
@@ -156,9 +155,7 @@ class DocumentAI:
             input_config = documentai.BatchDocumentsInputConfig(gcs_prefix=gcs_prefix)
 
         # Cloud Storage URI for the Output Directory
-        gcs_output_config = documentai.DocumentOutputConfig.GcsOutputConfig(
-            gcs_uri=gcs_output_uri, field_mask=field_mask
-        )
+        gcs_output_config = documentai.DocumentOutputConfig.GcsOutputConfig(gcs_uri=gcs_output_uri, field_mask=field_mask)
 
         # Where to write results
         output_config = documentai.DocumentOutputConfig(gcs_output_config=gcs_output_config)
@@ -166,19 +163,16 @@ class DocumentAI:
         if processor_version_id:
             # The full resource name of the processor version, e.g.:
             # projects/{PROJECT_ID}/locations/{LOCATION}/processors/{PROCESSOR_ID}/processorVersions/{processor_version_id}
-            name = client.processor_version_path(
-                PROJECT_ID, LOCATION, PROCESSOR_ID, processor_version_id
-            )
+            name = client.processor_version_path(PROJECT_ID, LOCATION, PROCESSOR_ID, processor_version_id)
         else:
             # The full resource name of the processor, e.g.:
             # projects/{PROJECT_ID}/locations/{LOCATION}/processors/{PROCESSOR_ID}
             name = client.processor_path(PROJECT_ID, LOCATION, PROCESSOR_ID)
 
-        request = documentai.BatchProcessRequest(
-            name=name,
-            input_documents=input_config,
-            document_output_config=output_config,
-        )
+        # Transcribes files
+        request = documentai.BatchProcessRequest(name=name, 
+                                                 input_documents=input_config,
+                                                 document_output_config=output_config,)
 
         # BatchProcess returns a Long Running Operation (LRO)
         operation = client.batch_process_documents(request)
@@ -216,10 +210,7 @@ class DocumentAI:
             # The Cloud Storage API requires the bucket name and URI prefix separately
             matches = re.match(r"gs://(.*?)/(.*)", process.output_gcs_destination)
             if not matches:
-                print(
-                    "Could not parse output GCS destination:",
-                    process.output_gcs_destination,
-                )
+                print("Could not parse output GCS destination:", process.output_gcs_destination,)
                 continue
 
             output_bucket, output_prefix = matches.groups()
@@ -231,9 +222,7 @@ class DocumentAI:
             for blob in output_blobs:
                 # Document AI should only output JSON files to GCS
                 if blob.content_type != "application/json":
-                    print(
-                        f"Skipping non-supported file: {blob.name} - Mimetype: {blob.content_type}"
-                    )
+                    print(f"Skipping non-supported file: {blob.name} - Mimetype: {blob.content_type}")
                     continue
 
                 # Download JSON File as bytes object and convert to Document Object
@@ -248,7 +237,3 @@ class DocumentAI:
                 # Read the text recognition output from the processor
                 print("The document contains the following text:")
                 print(document.text)
-
-                output_name = "./txt_files/" + file_name.split(".")[0] + output_type
-                with open(output_name, "w") as text_file:
-                    text_file.write(document.text)
