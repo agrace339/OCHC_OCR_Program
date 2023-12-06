b5a380ce466dec8ab18e83f57bdc3059042f9297
dkaruri <98552662+dkaruri@users.noreply.github.com>
2023-12-06 17:21:04 -0500

    Update batch_conversion.py

--- batch_conversion.py
+++ batch_conversion.py
@@ -12,84 +12,85 @@ def file_to_pdf(file):
 	#CITE: https://github.com/josch/img2pdf
 	#DESC: Open source code from IMG2PDF github
 	#Open input file and convert to pdf.
-	global cancelled
-	if cancelled:
-		return
-	match = re.search(r"(\w)+.(jpg|tif)", file)
-	if match:
-		title = (match.group())[0:-4] + ".pdf"
-		pdf_loc = file + ".pdf"
-	with open(title,"wb") as f:
-		f.write(img2pdf.convert(file))
-	return pdf_loc
+    global cancelled
+    if cancelled:
+        return
+    match = re.search(r"(\w)+.(jpg|tif)", file)
+    if match:
+        title = (match.group())[0:-4] + ".pdf"
+        pdf_loc = file + ".pdf"
+    with open(title,"wb") as f:
+        f.write(img2pdf.convert(file))
+    return pdf_loc
 
 def folder_to_pdf(folder_name):
 #Convert all JPEG files in a folder into one multi-page pdf file.
 	#Get current working directory.
-	img_dir = folder_name
+    img_dir = folder_name
 
 	#CITE: https://github.com/josch/img2pdf
 	#DESC: Open source code from IMG2PDF github
 	#Find all images within the directory.
-	global cancelled
-	if cancelled:
-		return
-	imgs = []
-	for r, _, f in os.walk(img_dir):
-		for fname in f:
-			if not fname.endswith(".jpg") and not fname.endswith(".tif"):
-				continue
-			imgs.append(os.path.join(r, fname))
-	folder_index = folder_name.rindex("/")
-	if folder_index != -1:
-		title = folder_name[folder_index+1:] + ".pdf"
-		pdf_loc = folder_name + ".pdf"
+    global cancelled
+    if cancelled:
+        return
+    imgs = []
+    for r, _, f in os.walk(img_dir):
+        for fname in f:
+            if not fname.endswith(".jpg") and not fname.endswith(".tif"):
+                continue
+            imgs.append(os.path.join(r, fname))
+    folder_index = folder_name.rindex("/")
+    if folder_index != -1:
+        title = folder_name[folder_index+1:] + ".pdf"
+        pdf_loc = folder_name + ".pdf"
 	#Converts all images in directory to pdfs.
-	with open(title, "wb") as f:
-		f.write(img2pdf.convert(imgs))
-	return pdf_loc
+    with open(title, "wb") as f:
+        f.write(img2pdf.convert(imgs))
+    return pdf_loc
 
 def convert(file):
 	#Check if cancelled is set True.
-	global cancelled
-	if cancelled:
-		return
+    global cancelled
+    if cancelled:
+        return
 	#Regular expressions to determine if input element is file or folder.
-	in_file = re.match(r"[/a-zA-Z0-9 ]+.(jpg|tif)", file)
-	in_folder = re.match(r"/[a-zA-Z0-9 ]+", file)
+    in_file = re.match(r"[/a-zA-Z0-9 ]+.(jpg|tif)", file)
+    in_folder = re.match(r"/[a-zA-Z0-9 ]+", file)
 
 	#If single file given, convert just that file to a pdf.
-	if in_file:
-		pdf_loc = file_to_pdf(file)
+    if in_file:
+        pdf_loc = file_to_pdf(file)
 	#If folder is given, convert all image files to pdfs in folder.
-	elif in_folder:
-		pdf_loc = folder_to_pdf(file)
-	#If argument is invalid, raise exception.
-	else:
-		raise Exception("Not valid file or folder name.")
-	return pdf_loc
+    elif in_folder:
+        pdf_loc = folder_to_pdf(file)
+    #If argument is invalid, raise exception.
+    else:
+        print(file)
+        raise Exception("Not valid file or folder name.")
+    return pdf_loc
 
 def set_cancelled():
 	#Function to set the 'cancelled' variable
-	cancelled = True
-	exit()
+    cancelled = True
+    exit()
 	#Relaunch program with saved list box elements
 
 def main(files):
-	global cancelled
-	if cancelled:
-		return
+    global cancelled
+    if cancelled:
+        return
 	#If no JPEG or TIFF file given, raise error.
-	if not files:
-		raise Exception("No file given.")
+    if not files:
+        raise Exception("No file given.")
 	
 	#Convert all files from list.
-	pdf_locs = []
-	files_completed = 0
-	for file in files:
-		pdf_locs.append(convert(file))
-		files_completed += 1
-	return pdf_locs
+    pdf_locs = []
+    files_completed = 0
+    for file in files:
+        pdf_locs.append(convert(file))
+        files_completed += 1
+    return pdf_locs
 
 if __name__ == '__main__':
-	main()
+    main()
